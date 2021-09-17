f46144e5bc83447b599f9fe13d322d340716be41
Alex Grintsvayg <git@grin.io>
2021-09-17 11:47:30 -0400

    accept DMs as well

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ import (
 	"github.com/slack-go/slack/socketmode"
 )
 
+var (
+	mySlackUsername     string
+	mySlackUserID       string
+	myTwitterScreenName string
+)
+
 func main() {
 	conf, err := loadConfig()
 	if err != nil {
@@ -34,28 +40,12 @@ func main() {
 		//socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
 	)
 
-	meSlack, err := slackApi.AuthTest()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "auth test err: %s", err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("I am %s on slack\n", meSlack.User)
-
 	twitterConf := oauth1.NewConfig(conf.TwitterConsumerKey, conf.TwitterConsumerSecret) // consumer key / secret
 	twitterToken := oauth1.NewToken(conf.TwitterAccessToken, conf.TwitterAccessSecret)   // access token / secret
 	httpClient := twitterConf.Client(oauth1.NoContext, twitterToken)
 	twitterClient := twitter.NewClient(httpClient)
 
-	meTwitter, _, err := twitterClient.Accounts.VerifyCredentials(&twitter.AccountVerifyParams{
-		SkipStatus:      twitter.Bool(true),
-		IncludeEmail:    twitter.Bool(false),
-		IncludeEntities: twitter.Bool(false),
-	})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "twitter verify credentials err: %s", err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("I am %s on twitter\n", meTwitter.ScreenName)
+	whoami(slackApi, twitterClient)
 
 	go func() {
 		for evt := range slackClient.Events {
@@ -83,42 +73,51 @@ func main() {
 					innerEvent := eventsAPIEvent.InnerEvent
 					switch ev := innerEvent.Data.(type) {
 					case *slackevents.AppMentionEvent:
-						if ev.User == meSlack.UserID {
+						if ev.User == mySlackUserID {
 							break // ignore bot's own messages
 						}
 
 						fmt.Printf("mentioned: %s\n", ev.Text)
-						text := ev.Text
-						prefix := fmt.Sprintf(`<@%s> `, meSlack.UserID)
-						if !strings.HasPrefix(text, prefix) {
+
+						prefix := fmt.Sprintf(`<@%s> `, mySlackUserID)
+						if !strings.HasPrefix(ev.Text, prefix) {
 							slackApi.SendMessage(ev.Channel, slack.MsgOptionText(
-								"Hi 👋. If you want me to tweet something, start your message with `@Poast `.", true))
+								"Hi 👋. If you want me to tweet something, start your message with `@Poast `.\n"+
+									"Feel free to make me tweet absolutely anything at all. Seriously, go wild. I won't mind.\n"+
+									"You can even DM me if you want full privacy.", true))
 							break
 						}
 
-						text = ev.Text[len(prefix):] + ` /c` // /c = communal
-
-						tweet, _, err := twitterClient.Statuses.Update(text, nil)
+						reply, err := tweet(twitterClient, ev.Text[len(prefix):])
 						if err != nil {
-							if apiError, ok := err.(twitter.APIError); ok && len(apiError.Errors) > 0 {
-								slackApi.SendMessage(ev.Channel, slack.MsgOptionText(
-									fmt.Sprintf("ERROR: %s", apiError.Errors[0].Message), true))
-							} else {
-								slackApi.SendMessage(ev.Channel, slack.MsgOptionText(
-									fmt.Sprintf("ERROR: %s", err.Error()), true))
-							}
+							slackApi.SendMessage(ev.Channel, slack.MsgOptionText("ERROR: "+err.Error(), true))
 							fmt.Printf("tweet err: %s\n", err.Error())
-							break
+						} else {
+							//log.Printf("https://twitter.com/%s/status/%s\n", meTwitter.ScreenName, tweet.IDStr)
+							slackApi.SendMessage(ev.Channel, slack.MsgOptionText(reply, true))
 						}
 
-						//log.Printf("https://twitter.com/%s/status/%s\n", meTwitter.ScreenName, tweet.IDStr)
-						slackApi.SendMessage(ev.Channel, slack.MsgOptionText(
-							fmt.Sprintf("poasted! https://twitter.com/%s/status/%s", meTwitter.ScreenName, tweet.IDStr), true))
-
-					case *slackevents.MemberJoinedChannelEvent:
-						//fmt.Printf("user %q joined to channel %q", ev.User, ev.Channel)
 					case *slackevents.MessageEvent:
-						//fmt.Printf("%s said %s\n", ev.User, ev.Text)
+						if ev.ChannelType != "im" || ev.User == mySlackUserID {
+							break // ignore bot's own messages
+						}
+
+						fmt.Printf("DMed: %s\n", ev.Text)
+
+						prefix := fmt.Sprintf(`<@%s> `, mySlackUserID)
+						text := ev.Text
+						if strings.HasPrefix(text, prefix) {
+							text = text[len(prefix):]
+						}
+
+						reply, err := tweet(twitterClient, text)
+						if err != nil {
+							slackApi.SendMessage(ev.Channel, slack.MsgOptionText("ERROR: "+err.Error(), true))
+							fmt.Printf("tweet err: %s\n", err.Error())
+						} else {
+							//log.Printf("https://twitter.com/%s/status/%s\n", meTwitter.ScreenName, tweet.IDStr)
+							slackApi.SendMessage(ev.Channel, slack.MsgOptionText(reply, true))
+						}
 					}
 				default:
 					slackClient.Debugf("unsupported Events API event received: %s", eventsAPIEvent.Type)
@@ -134,6 +133,45 @@ func main() {
 	}()
 
 	slackClient.Run()
+}
+
+func whoami(s *slack.Client, t *twitter.Client) {
+	meSlack, err := s.AuthTest()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "slack auth test err: %s", err.Error())
+		os.Exit(1)
+	}
+	mySlackUsername = meSlack.User
+	mySlackUserID = meSlack.UserID
+	fmt.Printf("I am %s on slack\n", mySlackUsername)
+
+	meTwitter, _, err := t.Accounts.VerifyCredentials(&twitter.AccountVerifyParams{
+		SkipStatus:      twitter.Bool(true),
+		IncludeEmail:    twitter.Bool(false),
+		IncludeEntities: twitter.Bool(false),
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "twitter verify credentials err: %s", err.Error())
+		os.Exit(1)
+	}
+	myTwitterScreenName = meTwitter.ScreenName
+	fmt.Printf("I am %s on twitter\n", myTwitterScreenName)
+}
+
+func tweet(client *twitter.Client, message string) (string, error) {
+	text := message + ` /c` // /c = communal
+
+	tweet, _, err := client.Statuses.Update(text, nil)
+	if err != nil {
+		if apiError, ok := err.(twitter.APIError); ok && len(apiError.Errors) > 0 {
+			return "", errors.New(apiError.Errors[0].Message)
+		} else {
+			return "", err
+		}
+	}
+
+	//log.Printf("https://twitter.com/%s/status/%s\n", meTwitter.ScreenName, tweet.IDStr)
+	return fmt.Sprintf("poasted! https://twitter.com/%s/status/%s", myTwitterScreenName, tweet.IDStr), nil
 }
 
 type config struct {
